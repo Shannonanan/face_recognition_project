bf5da2cc2dd781dbbc1c22bc95710c6fb613eb6c
Shannon Ferguson <shannonferguson@Shannons-MacBook-Pro.local>
2019-08-09 08:43:24 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import Clarifai from'clarifai';
 
-function App() {
+const app = new Clarifai.App({
+ apiKey: ''
+});
+
+const particlesOptions = {
+                particles: {
+                  number: {
+                    value: 30,
+                    density: {
+                      enable: true,
+                      value_area: 200
+                    }
+                  }
+                }
+              }
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+onInputChange = (event) => {
+  console.log(event.target.value);
+}
+
+onButtonSubmit = () => {
+  console.log('click');
+  app.models
+.predict(
+Clarifai.COLOR_MODEL,
+    // URL
+    "https://samples.clarifai.com/metro-north.jpg"
+)
+.then(function(response) {
+   console.log(response);
+    },
+    function(err) {
+      console.log('click');
+    }
+);
+}
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Particles className = 'particles'
+              params={particlesOptions}
+          
+            />
+    <Navigation/>
+    <Logo/>
+     <Rank/>
+    <ImageLinkForm  onInputChange= {this.onInputChange} 
+    onButtonSubmit = {this.onButtonSubmit}/>
+   
+   { 
+   	/*
+    
+    <FaceRecognition/>*/} 
+      
     </div>
   );
+}
 }
 
 export default App;
